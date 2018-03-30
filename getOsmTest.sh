7157c8a7f731df34e46cf3dddd7fd1b9d6012c36
Andrew Blomenberg <andrew_blomeberg@taylor.edu>
2018-03-30 12:12:36 -0600

    First draft of OSM API call scripts

---
+++ getOsmTest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Get the osm for populated places in Israel
+# This could be done in one wget but seperate steps are nicer
+wget -O cities.osm 'http://overpass-api.de/api/interpreter?data=[timeout:3600];node[place="city"][~"^is_in"~"Israel$"];out;'
+wget -O towns.osm 'http://overpass-api.de/api/interpreter?data=[timeout:3600];node[place="town"][~"^is_in"~"Israel$"];out;'
+wget -O villages.osm 'http://overpass-api.de/api/interpreter?data=[timeout:3600];node[place="village"][~"^is_in"~"Israel$"];out;'
+wget -O hamlets.osm 'http://overpass-api.de/api/interpreter?data=[timeout:3600];node[place="hamlet"][~"^is_in"~"Israel$"];out;'
+wget -O isolated.osm 'http://overpass-api.de/api/interpreter?data=[timeout:3600];node[place="isolated_dewlling"][~"^is_in"~"Israel$"];out;'
+
+# Merge the resulting files into one
+osmconvert cities.osm towns.osm villages.osm hamlets.osm isolated.osm -o=Israel.osm
+rm cities.osm towns.osm villages.osm hamlets.osm isolated.osm
